5edbf7f3fe8412f017b761632b374fe6795257c9
Natalija <lunasonne37@outlook.com>
2025-03-05 22:46:36 +0100

    commit 4

---
+++ src/DomShapes/Circle.java
@@ -0,0 +1,22 @@
+package DomShapes;
+
+public class Circle extends Shape {
+    private double radius;
+
+    public Circle(String title, double radius) {
+        super(title);
+        this.radius = radius;
+    }
+
+    @Override
+    public double calcArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "круг имеет радиус " + radius + " см";
+
+    }
+}
