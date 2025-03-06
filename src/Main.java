018fc22503b3aac8790b7dc1f5dfba59dc9c3b95
Natalija <lunasonne37@outlook.com>
2025-03-06 18:43:05 +0100

    commit Test3

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args)
-    {
-// 7777777
-    // ccc   fddd ccccc
-        System.out.println("Hello, World!");
-    }
-}
--- src/Test.java
+++
@@ -1,5 +0,0 @@
-public class Test {
-// commitTest в классе
-    String name;
-    String age;
-}
